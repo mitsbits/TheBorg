b866eb77f0f8e875aa8f2d050ef3e2e8e1a84fc9
mitsbits <mitsbits@gmail.com>
2016-12-30 21:02:39 +0200

    ext

---
+++ src/Infra.Core/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Borg
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime RoundUp(this DateTime dt, TimeSpan d)
+        {
+            return new DateTime(((dt.Ticks + d.Ticks - 1) / d.Ticks) * d.Ticks);
+        }
+
+        public static DateTime RoundDown(this DateTime dt, TimeSpan d)
+        {
+            return new DateTime(((dt.Subtract(d).Ticks + d.Ticks - 1) / d.Ticks) * d.Ticks);
+        }
+    }
+}
