71f10b7596879b99147c3d8cfacc7c1b7525a768
mitsbits <mitsbits@gmail.com>
2017-01-08 21:22:22 +0200

    trying to push to myget

--- build.bat
+++ build.bat
@@ -3,21 +3,15 @@ set config=%1
 if "%config%" == "" (
    set config=Release
 )
- 
-set version=1.0.0
+
+set version=
 if not "%PackageVersion%" == "" (
-   set version=%PackageVersion%
+   set version=-Version %PackageVersion%
 )
 
-set nuget=
-if "%nuget%" == "" (
-	set nuget=nuget
-)
+REM Build
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" MyGet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild MyGet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
-
+REM Package
 mkdir Build
-mkdir Build\lib
-mkdir Build\lib\net40
-
-%nuget% pack "MyGet.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+call %nuget% pack "src\Infra.Core\Infra.Core.xproj" -IncludeReferencedProjects -o Build -p Configuration=%config% %version%
