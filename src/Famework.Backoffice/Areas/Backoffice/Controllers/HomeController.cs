8104bdf20afdbc914b5b11ed50bba15a7560d24f
mitsbits <mitsbits@gmail.com>
2017-02-15 23:07:17 +0200

    added backoffice project

---
+++ src/Famework.Backoffice/Areas/Backoffice/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Borg.Famework.Backoffice.Areas.Backoffice.Controllers
+{
+    [Area("backoffice")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
