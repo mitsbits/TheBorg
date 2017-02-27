3e2b9f1528e6f2cc011417a9c7eaea041eb33ff3
mitsbits <mitsbits@gmail.com>
2017-02-27 22:38:01 +0200

    concatenate strings tag helper

---
+++ src/Infra.Core/Log/SuppressLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Borg.Infra.Core.Log
+{
+    public class SuppressLogger : ILogger
+    {
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return new NullScope();
+        }
+
+        public class NullScope : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
